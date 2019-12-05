427a3a7528e64ab32627e22dd1b048d2f2072348
Kenzo <presto12321@gmail.com>
2019-12-05 09:34:23 -0500

    correct ts interface

--- index.d.ts
+++ index.d.ts
@@ -3,20 +3,13 @@ import * as React from "react";
 declare class EditProfile extends React.Component<EditProfileProps, any> { }
 
 interface EditProfileProps {
-  box?: [any];
-  space?: [any];
+  box: [any];
+  space: [any];
+  currentUserAddr: string;
+
   currentUser3BoxProfile?: [any];
-
-  currentUserAddr?: string;
-
   redirectFn?: (ethAddr: string) => any;
-
   customFields?: [any][];
-  // customFields: Array<CustomFields>;
 }
-
-// interface CustomFields extends EditProfileProps {
-//   [index: number]: [any]
-// }
 
 export default EditProfile;
